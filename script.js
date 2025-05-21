79e530763468699a2ec994d24d54dafb2bf00983
Zp1021 <90364004+Zp1021@users.noreply.github.com>
2025-05-21 18:37:56 -0400

    Changed the basis for how the game is set up via the dom

--- script.js
+++ script.js
@@ -7,38 +7,27 @@ The rules of tic tac toe
 4. when 9 squares are filled and no player has 3 marks in a row game ends in a tie
 */
 
+//Accessing the body of the html file
 const body = document.querySelector("body")
 
-let div = document.createElement("div")
-div.classList.add('gameBoard')
-body.appendChild(div)
+// Creating a div with the class of gameBoard and adding it to the body
+let gameBoard = document.createElement("div")
+gameBoard.classList.add('gameBoard')
 
-let table = document.createElement("table")
-div.appendChild(table)
-
-for(i = 0; i < 3; i++){
-    let tableRow = document.createElement("tr")
-    table.appendChild(tableRow)
-    for (j = 0; j < 3; j++){
-        let td = document.createElement('td')
-        tableRow.appendChild(td)
-    }
+// Creating a table to add to the gameBoard Div
+// Creating the rows and squares for the tic tac toe board
+let game = ['','','','','','','','','']
+for (let move of game) {
+    let box = document.createElement("div")
+    box.textContent = move;
+    box.classList.add('box')
+    gameBoard.appendChild(box)  
 }
+body.appendChild(gameBoard)
 
-// let gameBoard = document.getElementsByClassName("gameBoard")
-// console.log(gameBoard)
-// // body.appendChild(gameBoard)
-// let table = document.createElement('table');
-// console.log(table)
-// gameBoard.appendChild(table);
 
-// for (i = 0; i < 3; i++) {
-//     let tableRow = document.createElement('tr');
-//     table.appendChild(tableRow);
-//     for(j = 0; j < 3; j++) {
-//         let td = document.createElement('td');
-//         tableRow.appendChild(td);
-//     }
-// }
+// User enters what they pattern they want to start as
+// variable that keeps track of current player
+// let player = x once turn happens the switch to player being O
 
 // const winCondition = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
